93d5d5337747d7688e73852c4e5b5f833bbf6208
Catherine Plisson <catwtac@gmail.com>
2024-07-01 11:18:09 +0200

    origin

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Tasks from './pages/Tasks';
+
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tasks" element={<Tasks />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tasks: React.FC = () => {
+  return (
+    <div className="container mx-auto mt-4"><p className="mt-2">Liste des tâches</p></div>
+  );
+}
+
+export default Tasks;
